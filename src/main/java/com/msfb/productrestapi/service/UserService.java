c47d240c625e2db2622846e58fdccc69755e8116
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-27 20:20:48 +0700

    fix bug for login using email or phoneNumber

--- src/main/java/com/msfb/productrestapi/service/UserService.java
+++ src/main/java/com/msfb/productrestapi/service/UserService.java
@@ -1,7 +1,6 @@
 package com.msfb.productrestapi.service;
 
 import com.msfb.productrestapi.dto.request.LoginRequest;
-import com.msfb.productrestapi.dto.request.LoginPhoneRequest;
 import com.msfb.productrestapi.dto.request.RegisterRequest;
 import com.msfb.productrestapi.dto.response.LoginResponse;
 import com.msfb.productrestapi.dto.response.RegisterResponse;
